40e01b6f977e074eb82763d1c80979cdb6191992
Phil Murgatroyd <45596387+ELFdev001@users.noreply.github.com>
2023-12-09 14:06:40 +0000

    Added basic font switching.
    Colour switching next

---
+++ Assets/Scripts/FontSwitcher.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class FontSwitcher : MonoBehaviour
+{
+    [SerializeField] TMP_FontAsset font1;
+    [SerializeField] TMP_FontAsset font2;
+    bool fontUsed;
+    TMP_Text[] thisText = new TMP_Text[5];
+    public GameObject[] textFields = new GameObject[5];
+    public InputAction fontToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 5; x++) {
+            thisText[x] = textFields[x].GetComponent<TMP_Text>();
+            thisText[x].font = font1;
+        }
+        fontUsed = true;
+    }
+
+    void OnEnable()
+    {
+        fontToggle.Enable();
+    }
+
+    void OnDisable()
+    {
+        fontToggle.Disable();
+    }
+
+    void SwitchFont()
+    {
+        if (fontUsed) {
+            for (int x = 0; x < 5; x++) {
+                thisText[x].font = font2;
+            }
+            fontUsed = false;
+        } else {
+            for (int x = 0; x < 5; x++) {
+                thisText[x].font = font1;
+            }
+            fontUsed = true;
+        }
+    }
+
+    void Update()
+    {
+        if (fontToggle.WasReleasedThisFrame())
+        {
+            SwitchFont();
+        }
+    }
+}
