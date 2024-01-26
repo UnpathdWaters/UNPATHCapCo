88b1e131e3466b8f5fc8418104bad6145f630dfe
ELFdev001 <45596387+ELFdev001@users.noreply.github.com>
2024-01-26 14:36:20 +0000

    Added initial different interpolation methods
    Some work left to do on the interface side

--- Assets/Scripts/SeaLevelServer.cs
+++ Assets/Scripts/SeaLevelServer.cs
@@ -8,6 +8,9 @@ public class SeaLevelServer : MonoBehaviour
 //    static readonly float[] SLC = new float[21]{ 0.6f, 0.8f, 1.1f, 1.5f, 1.7f, 2.4f, 3.3f, 7.8f, 8.8f, 8.1f, 13.3f, 5.6f, 4.7f, 14.4f, 14.2f, 3.2f, 4.0f, 3.6f, 1.2f, 0.6f, 0.0f };
     float[] SLC = new float[16] { -6.97f, -9.89f, -13.80f, -22.24f, -31.82f, -41.27f, -53.46f, -55.82f, -57.85f, -63.01f, -76.72f, -90.09f, -92.60f, -94.89f, -93.14f, -90.20f };
     TimeServer time;
+    [SerializeField] AnimationCurve slcLinearInt;
+    [SerializeField] AnimationCurve slcSteppedInt;
+    [SerializeField] AnimationCurve slcWigglyInt;
 
     void Awake()
     {
@@ -25,9 +28,20 @@ public class SeaLevelServer : MonoBehaviour
         if (time.GetYear() == 20000) {
             return SLC[SLC.Length - 1];
         } else {
+
+            float timeThroughCentury = time.GetYear() % 1000;
             int SLCindex = (time.GetYear() - 5000) / 1000;
+            float giaWaterHeight = Mathf.Lerp(SLC[SLCindex], SLC[SLCindex + 1], slcWigglyInt.Evaluate(timeThroughCentury / 1000.0f));
+
+
+
+/*            int SLCindex = (time.GetYear() - 5000) / 1000;
             float giaWaterHeight = SLC[SLCindex];
-            giaWaterHeight = giaWaterHeight - (((SLC[SLCindex] - SLC[SLCindex + 1]) / 1000) * (time.GetYear() % 1000));
+            giaWaterHeight = giaWaterHeight - (((SLC[SLCindex] - SLC[SLCindex + 1]) / 1000) * (time.GetYear() % 1000));*/
+
+
+
+
             return giaWaterHeight;
         }
     }
